3545695221d9f0910448e977bf6eba7c627ae3b5
marcos pinardi <exegeses@gmail.com>
2020-03-31 12:37:59 -0300

    cambio de color con setInterval

--- clase3/js/cambiarColor.js
+++ clase3/js/cambiarColor.js
@@ -6,12 +6,18 @@
     function cambiarColor()
     {
         //variables para las luces
-        var rojo = 25;
-        var verde = 52;
-        var azul = 200;
+        var rojo = Math.round( Math.random() * 255 );
+            console.log('r:' + rojo);
+        var verde = Math.round( Math.random() * 255 );
+            console.log('g:' + verde);
+        var azul = Math.round( Math.random() * 255 );
+            console.log('b:' + azul);
 
         // cambiar color
         caja.style.backgroundColor = 'rgb('+rojo+', '+verde+', '+azul+')';
     }
 
     cambiarColor();
+
+    setInterval( cambiarColor, 300 );
+
