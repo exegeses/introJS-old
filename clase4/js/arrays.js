98c1c3f86165d8cec0ba278e3ad07cb03fe5d3dd
marcos pinardi <exegeses@gmail.com>
2020-04-07 11:49:56 -0300

    Notas y teoria de arrays

---
+++ clase4/js/arrays.js
@@ -0,0 +1,21 @@
+// dos maneras de crear un array
+
+/*
+var nombre1 = 'Juan';
+var nombre2 = 'Lucas';
+var nombre3 = 'Mateo';
+var nombre4 = 'Marcos';
+*/
+
+//* usando el constructor new
+
+  /*  var nombres = new Array( 'Juan', 'Lucas', 'Mateo', 'Marcos',
+                                  'Santiago', 'Pedro' ); */
+
+//* usando corchetes
+
+    var nombres = [ 'Juan', 'Lucas', 'Mateo', 'Marcos',
+                        'Santiago', 'Pedro', 'Benjamin', 'Pablo' ];
+
+    console.log( nombres );
+    console.log( nombres[2] );
