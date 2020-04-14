55891611c3e58e9b3a54c23e9e143910cf9b3c37
marcos pinardi <exegeses@gmail.com>
2020-04-14 11:45:51 -0300

    querySelector

---
+++ clase5/js/ubicarElementos.js
@@ -0,0 +1,23 @@
+// ubicar elemento/s dentro del DOM
+//## usando su ID - elemento único
+
+var link = document.getElementById('link');
+
+    link.innerText = 'Elemento encontrado';
+
+//## usando su nombre de etiqueta - coleccion de HTML
+
+var links = document.getElementsByTagName('a');
+    console.log(links);
+    links[3].innerText = 'Elemento encontrado';
+var lis = document.getElementsByTagName('li');
+    lis[2].style.backgroundColor = '#e9e9e9';
+
+//## usando su nombre de clase - coleccion de HTML
+var articulos = document.getElementsByClassName('producto');
+    console.log(articulos);
+    articulos[2].style.backgroundColor = '#e9e9e9';
+    articulos[0].style.backgroundColor = '#e9e9e9';
+
+//## usando su selector
+
